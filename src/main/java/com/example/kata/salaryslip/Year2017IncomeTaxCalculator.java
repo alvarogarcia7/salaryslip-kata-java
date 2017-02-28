6e521736d92643978b267c41def24f61b62d518d
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 20:00:30 +0000

    Refactor: static import

--- src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
@@ -2,20 +2,21 @@ package com.example.kata.salaryslip;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.*;
+
 public class Year2017IncomeTaxCalculator implements IncomeTaxCalculator {
     @Override
     public BigDecimal taxableIncomeFor (final Employee employee) {
-        BigDecimal personalAllowance = BigDecimal.valueOf(11000);
+        BigDecimal personalAllowance = valueOf(11000);
         final BigDecimal grossAnnualSalary = employee.grossAnnualSalary();
-        final BigDecimal personalAllowanceThreshold = BigDecimal.valueOf(100_000);
+        final BigDecimal personalAllowanceThreshold = valueOf(100_000);
         if(firstIsGreaterThan(grossAnnualSalary, personalAllowanceThreshold)) {
-            personalAllowance = personalAllowance.subtract(grossAnnualSalary.subtract(personalAllowanceThreshold).divide(BigDecimal
-                    .valueOf(2)));
+            personalAllowance = personalAllowance.subtract(grossAnnualSalary.subtract(personalAllowanceThreshold).divide(valueOf(2)));
         }
         if (firstIsGreaterThan(grossAnnualSalary, personalAllowance)) {
             return grossAnnualSalary.subtract(personalAllowance);
         }
-        return BigDecimal.ZERO;
+        return ZERO;
     }
 
     private boolean firstIsGreaterThan (final BigDecimal first, final BigDecimal second) {
