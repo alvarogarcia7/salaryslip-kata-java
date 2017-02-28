5d806b9534a94dd002b62a5d4d9f89fe19650633
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 20:00:45 +0000

    Refactor: static import

--- src/test/java/com/example/kata/salaryslip/IncomeTaxCalculatorShould.java
+++ src/test/java/com/example/kata/salaryslip/IncomeTaxCalculatorShould.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.math.BigDecimal;
 
+import static java.math.BigDecimal.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -19,23 +20,23 @@ public class IncomeTaxCalculatorShould {
 
     @Test
     public void taxable_income_in_the_personal_allowance_tax_band () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(BigDecimal.ZERO)), is(BigDecimal.ZERO));
-        assertThat(sut.taxableIncomeFor(employeeMaking(BigDecimal.valueOf(11000))), is(BigDecimal.ZERO));
+        assertThat(sut.taxableIncomeFor(employeeMaking(ZERO)), is(ZERO));
+        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(11000))), is(ZERO));
     }
 
     @Test
     public void taxable_income_in_the_basic_rate_tax_band () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(BigDecimal.valueOf(12000))), is(BigDecimal.valueOf(1000)));
+        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(12000))), is(valueOf(1000)));
     }
 
     @Test
     public void taxable_income_under_100k () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(BigDecimal.valueOf(100_000))), is(BigDecimal.valueOf(89_000)));
+        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(100_000))), is(valueOf(89_000)));
     }
 
     @Test
     public void the_taxable_income_increases_by_reducing_the_personal_allowance () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(BigDecimal.valueOf(105_500))), is(BigDecimal.valueOf(97_250)));
+        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(105_500))), is(valueOf(97_250)));
     }
 
     private Employee employeeMaking (final BigDecimal grossAnnualSalary) {
