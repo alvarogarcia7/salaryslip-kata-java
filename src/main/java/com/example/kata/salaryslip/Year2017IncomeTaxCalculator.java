fc8ffdb9d40c8e388042257afdf2b5914f278f63
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 20:11:05 +0000

    Green

--- src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
@@ -12,6 +12,9 @@ public class Year2017IncomeTaxCalculator implements IncomeTaxCalculator {
         final BigDecimal personalAllowanceThreshold = valueOf(100_000);
         if(firstIsGreaterThan(grossAnnualSalary, personalAllowanceThreshold)) {
             personalAllowance = personalAllowance.subtract(grossAnnualSalary.subtract(personalAllowanceThreshold).divide(valueOf(2)));
+            if (firstIsGreaterThan(BigDecimal.ZERO, personalAllowance)) {
+                personalAllowance = BigDecimal.ZERO;
+            }
         }
         if (firstIsGreaterThan(grossAnnualSalary, personalAllowance)) {
             return grossAnnualSalary.subtract(personalAllowance);
