d4b408999f84b135599cf388e8f3d8d40e2b4d42
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 03:39:11 +0000

    Green

--- src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 public class Year2017IncomeTaxCalculator implements IncomeTaxCalculator {
     @Override
     public BigDecimal taxableIncomeFor (final Employee employee) {
-        return null;
+        return BigDecimal.ZERO;
     }
 }
