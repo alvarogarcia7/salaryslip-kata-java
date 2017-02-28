196a6651b90b193e690aa0a62198c308a25038fe
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 02:03:27 +0000

    Green

--- src/main/java/com/example/kata/salaryslip/Year2017NationalInsuranceContributionCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017NationalInsuranceContributionCalculator.java
@@ -5,6 +5,6 @@ import java.math.BigDecimal;
 public class Year2017NationalInsuranceContributionCalculator implements NationalInsuranceContributionCalculator {
     @Override
     public BigDecimal amountFor (final Employee employee) {
-        return null;
+        return BigDecimal.ZERO;
     }
 }
