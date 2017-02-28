94fb392f26e2e2f8e3d0e52ca865883e5f11595a
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 02:15:25 +0000

    Green: test for higher treshold for basic contributions

--- src/test/java/com/example/kata/salaryslip/NationalInsuranceContributionCalculatorShould.java
+++ src/test/java/com/example/kata/salaryslip/NationalInsuranceContributionCalculatorShould.java
@@ -20,18 +20,25 @@ public class NationalInsuranceContributionCalculatorShould {
 
     @Test
     public void calculate_for_no_contributions_band_lower_bound () {
-        assertThat(sut.amountFor(employeeMaking(BigDecimal.ZERO)),  is(ZERO));
+        assertThat(sut.amountFor(employeeMaking(BigDecimal.ZERO)), is(ZERO));
     }
 
     @Test
     public void calculate_for_no_contributions_band_higher_bound () {
-        assertThat(sut.amountFor(employeeMaking(BigDecimal.valueOf(8060.00))),  is(ZERO));
+        assertThat(sut.amountFor(employeeMaking(BigDecimal.valueOf(8060.00))), is(ZERO));
     }
 
     @Test
-    public void calculate_12_percent_for_contributions_in_the_basic_contributions_band () {
+    public void calculate_12_percent_for_contributions_in_the_basic_contributions_band_lower_treshold () {
         final BigDecimal basicContributions = BigDecimal.ONE.multiply(BigDecimal.valueOf(0.12));
         equalBigDecimalValues(sut.amountFor(employeeMaking(BigDecimal.valueOf(8061.00))), basicContributions);
+    }
+
+    @Test
+    public void calculate_12_percent_for_contributions_in_the_basic_contributions_band_upper_treshold () {
+        final BigDecimal basicContributionsAmount = BigDecimal.valueOf(43000).subtract(BigDecimal.valueOf(8060));
+        final BigDecimal basicContributions = basicContributionsAmount.multiply(BigDecimal.valueOf(0.12));
+        equalBigDecimalValues(sut.amountFor(employeeMaking(BigDecimal.valueOf(43000.00))), basicContributions);
     }
 
     private void equalBigDecimalValues (final BigDecimal actual, final BigDecimal expected) {
