9356122ced5530b8af91f5053f0d4cb9562593f8
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 02:22:28 +0000

    Green: scaffold - duplicate test implementation

--- src/main/java/com/example/kata/salaryslip/Year2017NationalInsuranceContributionCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017NationalInsuranceContributionCalculator.java
@@ -7,10 +7,19 @@ public class Year2017NationalInsuranceContributionCalculator implements National
     public BigDecimal amountFor (final Employee employee) {
         final BigDecimal annualGrossSalary = employee.grossAnnualSalary();
         final BigDecimal basicContributionsTreshold = BigDecimal.valueOf(8060.00);
+        if (firstIsGreaterThanSecond(annualGrossSalary, BigDecimal.valueOf(43000.00))) {
+
+            final BigDecimal higherContributions = annualGrossSalary.subtract(BigDecimal.valueOf(43000.00));
+            final BigDecimal higher = higherContributions.multiply(BigDecimal.valueOf(0.02));
+
+            final BigDecimal basic = BigDecimal.valueOf(43000).subtract(BigDecimal.valueOf(8060)).multiply(BigDecimal.valueOf(0.12));
+            return higher.add(basic);
+        }
         if (firstIsGreaterThanSecond(annualGrossSalary, basicContributionsTreshold)) {
             final BigDecimal basicContributions = annualGrossSalary.subtract(basicContributionsTreshold);
             return basicContributions.multiply(BigDecimal.valueOf(0.12));
         }
+
         return BigDecimal.ZERO;
     }
 
