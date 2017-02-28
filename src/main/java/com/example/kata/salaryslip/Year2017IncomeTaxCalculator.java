211eca436f85947ca92bf90615c4aadbb5c0855b
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 19:52:52 +0000

    Green

--- src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
@@ -5,6 +5,14 @@ import java.math.BigDecimal;
 public class Year2017IncomeTaxCalculator implements IncomeTaxCalculator {
     @Override
     public BigDecimal taxableIncomeFor (final Employee employee) {
+        final BigDecimal personalAllowance = BigDecimal.valueOf(11000);
+        if (firstIsGreaterThan(employee.grossAnnualSalary(), personalAllowance)) {
+            return employee.grossAnnualSalary().subtract(personalAllowance);
+        }
         return BigDecimal.ZERO;
+    }
+
+    private boolean firstIsGreaterThan (final BigDecimal first, final BigDecimal second) {
+        return first.compareTo(second) == 1;
     }
 }
