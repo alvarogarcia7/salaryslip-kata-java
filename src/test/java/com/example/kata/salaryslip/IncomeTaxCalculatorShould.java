e33817e372179d574c8a7f4d98c73de3c5e52632
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 20:06:11 +0000

    Refactor: work on the readability

--- src/test/java/com/example/kata/salaryslip/IncomeTaxCalculatorShould.java
+++ src/test/java/com/example/kata/salaryslip/IncomeTaxCalculatorShould.java
@@ -1,13 +1,15 @@
 package com.example.kata.salaryslip;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
-import static java.math.BigDecimal.*;
+import static java.math.BigDecimal.ZERO;
+import static java.math.BigDecimal.valueOf;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
 public class IncomeTaxCalculatorShould {
 
@@ -20,27 +22,35 @@ public class IncomeTaxCalculatorShould {
 
     @Test
     public void taxable_income_in_the_personal_allowance_tax_band () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(ZERO)), is(ZERO));
-        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(11000))), is(ZERO));
+        whenSalaryIs(ZERO, expectTaxableIncome(ZERO));
+        whenSalaryIs(valueOf(11000), expectTaxableIncome(ZERO));
     }
 
     @Test
     public void taxable_income_in_the_basic_rate_tax_band () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(12000))), is(valueOf(1000)));
+        whenSalaryIs(valueOf(12000), expectTaxableIncome(valueOf(1000)));
     }
 
     @Test
     public void taxable_income_under_100k () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(100_000))), is(valueOf(89_000)));
+        whenSalaryIs(valueOf(100_000), expectTaxableIncome(valueOf(89_000)));
     }
 
     @Test
     public void the_taxable_income_increases_by_reducing_the_personal_allowance () {
-        assertThat(sut.taxableIncomeFor(employeeMaking(valueOf(105_500))), is(valueOf(97_250)));
+        whenSalaryIs(valueOf(105_500), expectTaxableIncome(valueOf(97_250)));
     }
 
     private Employee employeeMaking (final BigDecimal grossAnnualSalary) {
         return new Employee("", "", grossAnnualSalary);
+    }
+
+    private Matcher<BigDecimal> expectTaxableIncome (final BigDecimal zero) {
+        return Is.is(zero);
+    }
+
+    private void whenSalaryIs (final BigDecimal grossAnnualSalary, final Matcher<BigDecimal> matcher) {
+        assertThat(sut.taxableIncomeFor(employeeMaking(grossAnnualSalary)), matcher);
     }
 
 }
