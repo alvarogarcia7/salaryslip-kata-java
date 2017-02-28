d7ebc2bbd9042609bcb18b68d685af40d800e71a
Alvaro Garcia <derecerca@gmail.com>
2017-02-28 20:13:46 +0000

    Refactor: extract explaining function

--- src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
@@ -10,17 +10,19 @@ public class Year2017IncomeTaxCalculator implements IncomeTaxCalculator {
         BigDecimal personalAllowance = valueOf(11000);
         final BigDecimal grossAnnualSalary = employee.grossAnnualSalary();
         final BigDecimal personalAllowanceThreshold = valueOf(100_000);
-        if(firstIsGreaterThan(grossAnnualSalary, personalAllowanceThreshold)) {
-            personalAllowance = personalAllowance.subtract(grossAnnualSalary.subtract(personalAllowanceThreshold).divide(valueOf(2)));
-            personalAllowance = personalAllowanceCannotBeNegative(personalAllowance);
-        }
-        if (firstIsGreaterThan(grossAnnualSalary, personalAllowance)) {
-            return grossAnnualSalary.subtract(personalAllowance);
-        }
-        return ZERO;
+        personalAllowance = reducePersonalAllowance(personalAllowance, grossAnnualSalary, personalAllowanceThreshold);
+        return getNumberOrZero(grossAnnualSalary.subtract(personalAllowance));
     }
 
-    private BigDecimal personalAllowanceCannotBeNegative (BigDecimal personalAllowance) {
+    private BigDecimal reducePersonalAllowance (BigDecimal personalAllowance, final BigDecimal grossAnnualSalary, final BigDecimal personalAllowanceThreshold) {
+        if(firstIsGreaterThan(grossAnnualSalary, personalAllowanceThreshold)) {
+            personalAllowance = personalAllowance.subtract(grossAnnualSalary.subtract(personalAllowanceThreshold).divide(valueOf(2)));
+            personalAllowance = getNumberOrZero(personalAllowance);
+        }
+        return personalAllowance;
+    }
+
+    private BigDecimal getNumberOrZero (BigDecimal personalAllowance) {
         if (firstIsGreaterThan(BigDecimal.ZERO, personalAllowance)) {
             personalAllowance = BigDecimal.ZERO;
         }
