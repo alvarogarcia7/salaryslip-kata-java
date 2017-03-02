b9989ed0dbc75f5d876b5bb56795329b197add03
Alvaro Garcia <derecerca@gmail.com>
2017-03-02 07:59:02 +0000

    green

--- src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
+++ src/main/java/com/example/kata/salaryslip/Year2017IncomeTaxCalculator.java
@@ -67,6 +67,7 @@ public class Year2017IncomeTaxCalculator implements IncomeTaxCalculator {
             this.add(new TaxBand(valueOf(0), ZERO));
             this.add(new TaxBand(valueOf(11_000).subtract(initial), valueOf(0.2)));
             this.add(new TaxBand(valueOf(43_000).subtract(initial), valueOf(0.4)));
+            this.add(new TaxBand(valueOf(150_000), valueOf(0.45)));
         }};
         Collections.reverse(taxBands);
         return taxBands;
